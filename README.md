cad594531115c1bf682fea7bc660a7e4506fd613
Type9 <gabespersonal@gmail.com>
2020-12-12 17:06:24 +0800

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# twatter-nodepackage
-A packaged version of my python-based tweet generation
+#  Twatter - Node
+
+[![NPM version](https://img.shields.io/npm/v/tocsify.svg?style=flat)](https://www.npmjs.com/package/tocsify) [![NPM downloads](https://img.shields.io/npm/dm/tocsify.svg?style=flat)](https://npmjs.org/package/tocsify) [![Build Status](https://img.shields.io/travis/droxey/tocsify.svg?style=flat)](https://travis-ci.org/droxey/tocsify)
+
+npm module that generates tweets using the markov chain model from a Twitter user handles
+
+## Features
+
+* Given a user handle, scrapes Twitter for a variable amount of tweets
+* Stores samples Tweets such that they don't need to be rescraped
+* Generates a random Tweet from sampled Tweets that attempt to resemble something that might be said by target user
+
+## Installation
+
+```bash
+npm install -g twatter-node
+```
+
+## Usage
+
+```bash
+Usage
+
+    generate([user], [sampleSize], [order], [overwrite]);
+
+
+Argument Details
+
+    user (String) -- Handle of target twitter user
+
+    sampleSize (integer) -- Number of tweets to sample if no previous samples exist
+
+    order -- Adjusts complexity of the model. Lowest should be 1 and it should be raised depending on how much text there is to sample. In general higher complexity will closer resemble human speech but has a higher chance of repeating something already said instead of being original.
+    
+    overwrite (boolean) -- Forces to ignore previously sampled Tweets and resamples at the given sampleSize.
+```
--- generate.js
+++ generate.js
@@ -1,38 +1,25 @@
 var {PythonShell} = require('python-shell');
 
-//Configs run parameters
-let config = (user, sampleSize, markovOrder) => {
+//Configs generation parameters
+let gen_config = (user, sampleSize, markovOrder, overwrite) => {
     return {
         mode: 'text',
         pythonOptions: ['-u'], // get print results in real-time
         scriptPath: './dependencies',
-        args: [user, sampleSize, markovOrder, false]
+        args: [user, sampleSize, markovOrder, overwrite]
     };
-}
+};
 
 //Configs generation
-let generate = (user) => {
-    let gen_options = config(user);
+let generate = (user, sampleSize, markovOrder, overwrite) => {
+    let gen_options = gen_config(user,sampleSize, markovOrder, overwrite);
 
     PythonShell.run('tweet_markov_gen.py', gen_options, (err, results) => {
         if (err) throw err;
-
         let tweet = new Tweet();
         tweet.text = results[0];
-        tweet
-            .save()
-            .then(tweet => {
-                return Promise.all([Handle.findOne({name: req.params.handle}), tweet]);
-            })
-            .then(([handle, tweet]) => { // bind the tweet to the handle and redirect
-                console.log("HANDLE: " + handle.name);
-                handle.tweets.unshift(tweet._id);
-                handle.save();
-                res.status(201);
-                res.redirect(`/${handle.name}/twatted/${tweet._id}`);
-            })
-            .catch(err => {
-                console.log(err.message);
-            });
+        return tweet.text;
     });
 };
+
+module.exports = {generate};
